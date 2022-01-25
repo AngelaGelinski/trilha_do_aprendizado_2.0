b115b8f630d994c97d4b0a5e3125c942a8225155
Angela Maria Gelinski <angegelinski@gmail.com>
2022-01-25 16:10:30 -0300

    att

---
+++ Lista_de_exercicios_Java/ex1.js
@@ -0,0 +1,8 @@
+/*Crie um script em que seja possível digitar a temperatura em Celsius, e seja exibida a
+temperatura convertida em Fahrenheit e Kelvin*/
+var tempC, tempF, tempK
+
+escreva( 'Qual é a temperatura em Celsius')
+leia(tempC)
+
+tempF= tempC*1.8 +32
