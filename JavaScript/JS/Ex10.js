5ac73b5433332a2e8cfa8da4bdc1458f1a3b7917
Angela Maria Gelinski <angegelinski@gmail.com>
2022-01-26 14:52:51 -0300

    att

---
+++ JavaScript/JS/Ex10.js
@@ -0,0 +1,19 @@
+var v=[4, 8, 7, 6]
+
+alert(v)
+
+alert(v[2])
+v[1=3]
+alert(v)
+
+var soma
+soma=v[0]+ v[3]
+alert(soma)
+
+lista=["Juquinha", 1910,"Rua a", "Irati", "Pr", 88.5]
+alert(lista)
+aler(lista[3])
+
+for(var i=0;i<lista.legnht;i++){
+document.write("<h2>" +lista[i]+"</h2>")
+}
