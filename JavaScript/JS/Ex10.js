c35982707d435c01c6f078a501a957d25a079bc5
Angela Maria Gelinski <angegelinski@gmail.com>
2022-01-26 15:15:07 -0300

    att

--- JavaScript/JS/Ex10.js
+++ JavaScript/JS/Ex10.js
@@ -17,5 +17,18 @@ aler(lista[3])
 for(var i=0;i<lista.legnht;i++){
 document.write("<h2>" +lista[i]+"</h2>")
 }
-lista.push("84500-000")
+lista.push("84500-000")//adiciona um elemento no final da lista
+lista.push("teste")
 alert(lista)
+
+lista.pop()//remove o ultimo elemento da lista
+alert(lista)
+
+lista.shift()//remove o primeiro elemento da lista
+alert(lista)
+lista.unshift("Zequinha")
+
+var posicao
+
+posicao=lista.indexOf(" Irati")
+alert(posicao)
