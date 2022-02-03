f1fe296f887308a26f6ff7ca576a3a8d928c18af
Angela Maria Gelinski <angegelinski@gmail.com>
2022-02-03 14:58:35 -0300

    attt

---
+++ JavaScript/JS/EX11.js
@@ -0,0 +1,25 @@
+/*funções sem retorno*/
+function somar(n1, n2){
+    var  r
+
+    r=n1+n2
+
+    alert("Função de soma sem retorno. O resultado de"+n1+"+"+n2+ "="+r)
+}
+
+/* Funções com retornos */
+
+function subtrair(n1,n2){
+    var r
+    r=n1-n2
+  
+    return(r)
+}
+
+somar(10,35)
+
+var resultado 
+
+resultado= subtrair (80,65)
+
+alert("Função da subtração sem retorno. O resultado de"+80+"-"+65+ "="+resultado)
