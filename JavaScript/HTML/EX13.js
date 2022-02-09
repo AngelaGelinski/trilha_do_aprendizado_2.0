6f4e97a8c5859325d650246b8f05d0a263a5a66f
Angela Maria Gelinski <angegelinski@gmail.com>
2022-02-09 14:54:45 -0300

    atualização

---
+++ JavaScript/HTML/EX13.js
@@ -0,0 +1,10 @@
+function somar(){
+ var n1=parseFloat( document.getElementById("n1").value)
+ var n2=parseFloat(document.getElementByI("n2").value)
+ var resultado 
+ resultado=n1+n2
+
+
+       document.getElementById("resultado").innerHTML=` O resultado da soma é ${resultado}`
+
+}
