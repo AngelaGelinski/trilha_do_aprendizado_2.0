faccf11565d06782e9d68a794c954596f993a786
Angela Maria Gelinski <angegelinski@gmail.com>
2022-02-09 15:08:40 -0300

    atualização

--- JavaScript/JS/EX13.js
+++ JavaScript/JS/EX13.js
@@ -1,6 +1,6 @@
 function somar(){
  var n1=parseFloat( document.getElementById("n1").value)
- var n2=parseFloat(document.getElementByI("n2").value)
+ var n2=parseFloat(document.getElementById("n2").value)
  var resultado 
  resultado=n1+n2
 
